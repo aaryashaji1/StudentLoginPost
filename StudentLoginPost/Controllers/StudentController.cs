279a6e0309398de9f6c2bbaf2f3e5ca4383b9533
aaryashaji1 <aaryashajit@gmail.com>
2021-05-03 08:12:27 -0500

    Add project files.

---
+++ StudentLoginPost/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using StudentLoginPost.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentLoginPost.Controllers
+{
+    public class StudentController : ApiController
+    {
+
+        String connectionString = "Server=ARYASHAJI\\SQLEXPRESS;Database=PersonManagement;Trusted_Connection=True;";
+        [HttpPost]
+        public Student StudentLogin(Student student)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("StudentLogin", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Username", student.Username);
+            command.Parameters.AddWithValue("Password", student.Password);
+
+
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            
+            student.Id = Convert.ToInt32(reader["Id"]);
+            student.Age = Convert.ToInt32(reader["Age"]);
+            student.Name = reader["Name"].ToString();
+            student.Place = reader["Place"].ToString();
+
+            reader.Close();
+            connection.Close();
+            return student;
+        }
+    }
+}
+
---
+++ StudentLoginPost/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentLoginPost.Models
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Place { get; set; }
+    }
+}
